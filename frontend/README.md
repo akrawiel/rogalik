0b564da078868a4733bde93716147b7886d88eac
Aleksander Krawiel <aleksander.krawiel@makimo.pl>
2020-07-16 17:49:24 +0200

    Initial commit

---
+++ frontend/README.md
@@ -0,0 +1,3 @@
+# Time Fort
+
+Everything is WIP :)
---
+++ frontend/src/App.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+  export let name: string;
+</script>
+
+<style global lang="sass">
+  html,
+  body
+    font-family: 'Varta', sans-serif
+
+  h1
+    color: #ff3e00
+</style>
+
+<main>
+  <h1>Hello {name}!</h1>
+  <p>
+    Visit the
+    <a href="https://svelte.dev/tutorial">Svelte tutorial</a>
+    to learn how to build Svelte apps.
+  </p>
+  <button>test</button>
+</main>
+
